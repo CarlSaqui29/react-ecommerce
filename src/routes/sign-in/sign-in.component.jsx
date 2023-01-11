79bc8f65d550349c3e791717cfa0b2a2c975282f
Carl Saqui <carlsaqui000@gmail.com>
2023-01-11 16:03:15 +0800

    added function enabling user popup signIn

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,17 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup()
+    console.log(response)
+  }
   return(
-    <h1>SignIn</h1>
+    <>
+      <h1>SignIn</h1>
+      <button onClick={logGoogleUser}>
+        Sign in with Google Popup
+      </button>
+    </>
   )
 }
 
